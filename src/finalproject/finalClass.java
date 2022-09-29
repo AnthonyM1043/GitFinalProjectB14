5e7c08ddb6a59a14e4dd363d2dc54a247bfbc297
AnthonyM1043 <anthony.martinez1043@gmail.com>
2022-09-28 20:14:49 -0400

    first commit

---
+++ src/finalproject/finalClass.java
@@ -0,0 +1,10 @@
+package finalproject;
+
+public class finalClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is the code for the mercedes");
+	}
+
+}
